cd01112e88de91a3a9f076290cddc209ef8cd7ff
abdelrahmanfekri <abdelrahmanfekrei49@gmail.com>
2024-03-24 00:38:47 +0200

    check

--- backend/settings.py
+++ backend/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-DATA_UPLOAD_MAX_NUMBER_FILES = 1000
+# DATA_UPLOAD_MAX_NUMBER_FILES = 1000
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
